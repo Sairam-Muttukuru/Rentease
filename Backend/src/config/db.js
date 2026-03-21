aa05f5808ef97986ef7ee20bf069189f69ec626e
Sairam-Muttukuru <bhavanimuttukuru@gmail.com>
2026-03-21 16:44:54 +0530

    ok

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -20,25 +20,27 @@
 
 // module.exports = pool;
 require("dotenv").config({ path: require("path").join(__dirname, "..", ".env") });
+
 const { Pool } = require("pg");
 
-const isProduction = process.env.DATABASE_URL;
+// Check if running in production (Render)
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool(
     isProduction
         ? {
-              connectionString: process.env.DATABASE_URL,
-              ssl: {
-                  rejectUnauthorized: false
-              }
-          }
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        }
         : {
-              user: process.env.DB_USER,
-              host: process.env.DB_HOST,
-              database: process.env.DB_DATABASE,
-              password: process.env.DB_PASSWORD,
-              port: process.env.DB_PORT
-          }
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
+        }
 );
 
 module.exports = pool;
